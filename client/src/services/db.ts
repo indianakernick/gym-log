74b38fabf2a88dd31b1d2011d6afc0078eff21ea
Indiana Kernick <indiana.kernick@gmail.com>
2023-01-24 11:46:35 +1030

    Allow successful uploads to be applied locally
    Also added some comments

--- client/src/services/db.ts
+++ client/src/services/db.ts
@@ -8,6 +8,30 @@ import {
 import { AsyncInit } from '../utils/async-init';
 import type { Exercise, Measurement, UserChanges, Workout } from './user';
 
+// When the user is making changes to their data, the changes are written to a
+// staging area. The staging area stores the difference between the local copy
+// of remote database (the canonical database) and the local database. The user
+// always sees the local version of the database (unless they're resolving merge
+// conflicts) which means that they see the canonical version with the staged
+// changes applied.
+//
+// Changes from the staging area will be uploaded from time to time. If a
+// different client made changes to the remote database since this client last
+// fetched it, then the request to apply changes will be rejected and the client
+// will need to fetch the remote changes. If the request to push changes to the
+// remote succeeds, then the change is removed the from the staging area and
+// applied to the canonical version. The version number is also incremented so
+// that the client knows the version of the remote database that is has.
+//
+// When a request to apply changes to the remote is rejected, the client will
+// fetch the new set of changes to be applied to its canonical database. At this
+// point, there will be two sets of changes relative to the canonical database.
+// If the sets of changes involve different objects, then they can be trivially
+// merged. However, if the same object is referenced twice (perhaps modified in
+// one and deleted in the other), then the user will be asked which change
+// should be applied and the merging process can start again with these
+// choices.
+
 interface Schema extends DBSchema {
   auth: {
     key: 'refresh_token';
@@ -71,14 +95,30 @@ export type MergeConflictResolutions = {
   [key: string]: 'local' | 'remote';
 };
 
-export interface StagedChanges {
+export type StagedChange = {
   version: number;
-  measurements: (Measurement | Deleted)[];
-  workouts: (Omit<Workout, 'exercises'> | Deleted)[];
-  exercises: (Exercise | Deleted)[];
-}
+} & ({
+  measurementId: string;
+  measurement: Measurement | Deleted;
+} | {
+  workoutId: string;
+  workout: Omit<Workout, 'exercises'> | Deleted;
+} | {
+  workoutId: string;
+  exerciseId: string;
+  exercise: Exercise | Deleted;
+});
+
+type StagedStores = {
+  measurement: 'stagedMeasurement',
+  workout: 'stagedWorkout',
+  exercise: 'stagedExercise',
+};
 
 export default new class {
+  // It would probably make more sense to have an async factory function instead
+  // of checking and waiting for the initialisation of this object every time
+  // it's accessed.
   private db = new AsyncInit<IDBPDatabase<Schema>>();
 
   constructor() {
@@ -103,6 +143,8 @@ export default new class {
     // navigator.storage.persist()
   }
 
+  // ---------------------------- Authentication ---------------------------- //
+
   async getRefreshToken(): Promise<string | undefined> {
     const db = await this.db.get();
     return db.get('auth', 'refresh_token');
@@ -113,10 +155,13 @@ export default new class {
     await db.put('auth', refreshToken, 'refresh_token');
   }
 
-  // The next six methods are used when the user is making changes to their data
+  // ----------------------- Stage changes for upload ----------------------- //
+  //
+  // Stage changes to the local database and prepare them to be uploaded to the
+  // remote database.
 
   stageDeleteMeasurement(measurementId: string): Promise<void> {
-    return this.stageDeletion('measurement', 'stagedMeasurement', measurementId);
+    return this.stageDelete('measurement', 'stagedMeasurement', measurementId);
   }
 
   async stageUpdateMeasurement(measurement: Measurement): Promise<void> {
@@ -125,7 +170,7 @@ export default new class {
   }
 
   stageDeleteWorkout(workoutId: string): Promise<void> {
-    return this.stageDeletion('workout', 'stagedWorkout', workoutId);
+    return this.stageDelete('workout', 'stagedWorkout', workoutId);
   }
 
   async stageUpdateWorkout(workout: Omit<Workout, 'exercises'>): Promise<void> {
@@ -134,7 +179,7 @@ export default new class {
   }
 
   stageDeleteExercise(workoutId: string, exerciseId: string): Promise<void> {
-    return this.stageDeletion('exercise', 'stagedExercise', `${workoutId}#${exerciseId}`);
+    return this.stageDelete('exercise', 'stagedExercise', `${workoutId}#${exerciseId}`);
   }
 
   async stageUpdateExercise(workoutId: string, exercise: Exercise): Promise<void> {
@@ -142,13 +187,10 @@ export default new class {
     await db.put('stagedExercise', exercise, `${workoutId}#${exercise.exercise_id}`);
   }
 
-  private async stageDeletion<
-    Canon extends StoreNames<Schema>,
-    Staged extends StoreNames<Schema>
-  >(
-    canon: Canon,
-    staged: Staged,
-    id: Schema[Canon]['key'] & Schema[Staged]['key']
+  private async stageDelete<S extends keyof StagedStores>(
+    canon: S,
+    staged: StagedStores[S],
+    id: Schema[S]['key']
   ): Promise<void> {
     const db = await this.db.get();
     const tx = db.transaction([canon, staged], 'readwrite');
@@ -162,9 +204,12 @@ export default new class {
     tx.commit();
   }
 
-  // Called after receiving a response from GET /user
-  // Could happen after periodically polling or after a modification request was
-  // rejected
+  // --------------- Merge remote changes with staged changes --------------- //
+  //
+  // Merge a set of remote changes into the staged changes. This could happen
+  // after periodically polling or after a modification request was rejected.
+  // The user will be asked to resolve merge conflicts if they arise.
+
   async merge(
     remote: UserChanges,
     resolutions: MergeConflictResolutions = {}
@@ -176,33 +221,79 @@ export default new class {
     return [];
   }
 
-  // get the staged changes to prepare for upload
-  async getStagedChanges(): Promise<StagedChanges> {
+  // --------------------- Get a staged change to upload -------------------- //
+  //
+  // Individual changes are uploaded one at a time. After uploading, the changes
+  // are applied. When preparing to upload the next change, the staged changes
+  // could have been changed by a user action so it needs to be read again.
+
+  async getNextStagedChange(): Promise<StagedChange | undefined> {
+    // go through each of the object stores and return the first change that is
+    // found
     throw 'todo';
   }
 
-  // uploading a staged change has succeeded so move the data out of the staged
-  // object stores. the staged entities might have changed while they were being
-  // uploaded. in that case, they should remain in the staging area.
-  // uploads will happen from one place (service worker) but the staging area
-  // could change from multiple tabs.
+  // ------------- Apply remote changes after successful upload ------------- //
+  //
+  // When uploading a change succeeds, the version number is incremented and the
+  // change is applied to the canonical version. If the change that was uploaded
+  // is still staged and hasn't changed, then the change is removed from the
+  // staging area.
 
-  // could put a modification time or a UUID on staged items whenever they change
-  // when they finish uploading, the tag can be compared and only if the tags differ
-  // will the item be removed from the staging area. it's possible that the item
-  // could be completely removed from the staging area, in that case, a DELETED
-  // object will need to be put in its place
-
-  async applyDeleteMeasurement(measurementId: string): Promise<void> {
-    return this.applyDeletion('measurement', 'stagedMeasurement', measurementId);
+  applyDeleteMeasurement(measurementId: string): Promise<void> {
+    return this.applyDelete('measurement', 'stagedMeasurement', measurementId);
   }
 
-  async applyDeleteWorkout(workoutId: string): Promise<void> {
-    return this.applyDeletion('workout', 'stagedWorkout', workoutId);
+  applyUpdateMeasurement(measurement: Measurement): Promise<void> {
+    return this.applyUpdate(
+      'measurement',
+      'stagedMeasurement',
+      measurement,
+      measurement.measurement_id,
+      (a, b) => {
+        return a.type === b.type
+          && a.capture_date === b.capture_date
+          && a.value === b.value
+          && a.notes === b.notes;
+      }
+    )
   }
 
-  async applyDeleteExercise(workoutId: string, exerciseId: string): Promise<void> {
-    return this.applyDeletion('exercise', 'stagedExercise', `${workoutId}#${exerciseId}`);
+  applyDeleteWorkout(workoutId: string): Promise<void> {
+    return this.applyDelete('workout', 'stagedWorkout', workoutId);
+  }
+
+  applyUpdateWorkout(workout: Omit<Workout, 'exercises'>): Promise<void> {
+    return this.applyUpdate(
+      'workout',
+      'stagedWorkout',
+      workout,
+      workout.workout_id,
+      (a, b) => {
+        return a.start_time === b.start_time
+          && a.finish_time === b.finish_time
+          && a.notes === b.notes;
+      }
+    );
+  }
+
+  applyDeleteExercise(workoutId: string, exerciseId: string): Promise<void> {
+    return this.applyDelete('exercise', 'stagedExercise', `${workoutId}#${exerciseId}`);
+  }
+
+  applyUpdateExercise(workoutId: string, exercise: Exercise): Promise<void> {
+    return this.applyUpdate(
+      'exercise',
+      'stagedExercise',
+      exercise,
+      `${workoutId}#${exercise.exercise_id}`,
+      (a, b) => {
+        return a.order === b.order
+          && a.type === b.type
+          && a.notes === b.notes
+          && JSON.stringify(a.sets) === JSON.stringify(b.sets);
+      }
+    );
   }
 
   private async incrementVersion<
@@ -213,26 +304,54 @@ export default new class {
     await store.put(version + 1, 'version');
   }
 
-  private async applyDeletion<
-    Canon extends StoreNames<Schema>,
-    Staged extends StoreNames<Schema>
-  >(
-    canon: Canon,
-    staged: Staged,
-    id: Schema[Canon]['key'] & Schema[Staged]['key']
+  private async applyDelete<S extends keyof StagedStores>(
+    canon: S,
+    staged: StagedStores[S],
+    id: Schema[S]['key']
   ): Promise<void> {
     const db = await this.db.get();
     const tx = db.transaction(['user', canon, staged], 'readwrite');
     const stagedStore = tx.objectStore(staged);
 
-    this.incrementVersion(tx);
+    await this.incrementVersion(tx);
 
     const stagedItem = stagedStore.get(id);
     if (stagedItem && 'deleted' in stagedItem) {
-      stagedStore.delete(id);
+      await stagedStore.delete(id);
     }
 
-    tx.objectStore(canon).delete(id);
+    await tx.objectStore(canon).delete(id);
     tx.commit();
+  }
+
+  private async applyUpdate<S extends keyof StagedStores>(
+    canon: S,
+    staged: StagedStores[S],
+    item: Schema[S]['value'],
+    id: Schema[S]['key'],
+    equal: (a: Exclude<Schema[StagedStores[S]]['value'], Deleted>, b: Schema[S]['value']) => boolean
+  ): Promise<void> {
+    const db = await this.db.get();
+    const tx = db.transaction(['user', canon, staged], 'readwrite');
+    const stagedStore = tx.objectStore(staged);
+
+    await this.incrementVersion(tx);
+
+    const stagedItem = await stagedStore.get(id);
+    if (!stagedItem) {
+      // The item was removed from the staging area entirely because it wasn't
+      // in the canonical version. So we need to add a tombstone since it now
+      // exists but needs to be removed.
+      await stagedStore.put(DELETED, id);
+    } else if (
+      !('deleted' in stagedItem)
+      // Despite narrowing away the `deleted` property, the compiler doesn't see
+      // that the resultant type has the `Deleted` union member excluded.
+      && equal(stagedItem as Exclude<Schema[StagedStores[S]]['value'], Deleted>, item)
+    ) {
+      await stagedStore.delete(id);
+    }
+
+    await tx.objectStore(canon).put(item);
   }
 }
