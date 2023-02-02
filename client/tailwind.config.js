a73d8055e1ace320d1f5084dff2ca8829b067435
Indiana Kernick <indiana.kernick@gmail.com>
2023-02-02 11:30:34 +1030

    Use TailwindCSS

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{css,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
