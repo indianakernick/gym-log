5a764fed1a8d25a7bae348d0e02ab4aa62e01229
Indiana Kernick <indiana.kernick@gmail.com>
2023-02-04 17:03:16 +1030

    Group measurements by year

--- client/src/views/MeasurementListView.vue
+++ client/src/views/MeasurementListView.vue
@@ -10,10 +10,27 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-const dates = shallowRef<string[]>([]);
+const years = shallowRef<string[][]>([]);
 
 db.getMeasurementDates().then(d => {
-  dates.value = d;
+  const groups: string[][] = [];
+
+  if (d.length > 0) {
+    let previous = d[0].substring(0, 4);
+
+    for (let i = 1; i < d.length; ++i) {
+      const current = d[i].substring(0, 4);
+      if (current !== previous) {
+        groups.push(d.splice(0, i));
+        previous = current;
+        i = 0;
+      }
+    }
+
+    groups.push(d);
+  }
+
+  years.value = groups;
 });
 
 function addToday() {
@@ -55,19 +72,27 @@ function addPast(event: Event) {
   </Header>
 
   <Main>
-    <ol class="my-2 mx-3">
+    <ol>
       <li
-        v-for="date in dates"
-        class="border-t border-r last:border-b border-l first:rounded-t-lg
-          last:rounded-b-lg dark:border-neutral-600 dark:bg-neutral-800"
+        v-for="year in years"
+        class="py-2 mx-3"
+        :aria-label="year[0].substring(0, 4)"
       >
-        <button
-          @click="router.push(`/measurements/${date}`)"
-          class="px-3 py-2 w-full flex justify-between items-center"
-        >
-          {{ displayDate(date) }}
-          <ChevronRightIcon class="w-5 h-5 text-neutral-500" />
-        </button>
+        <ol>
+          <li
+            v-for="date in year"
+            class="border-t border-r last:border-b border-l first:rounded-t-lg
+              last:rounded-b-lg dark:border-neutral-600 dark:bg-neutral-800"
+          >
+            <button
+              @click="router.push(`/measurements/${date}`)"
+              class="px-3 py-2 w-full flex justify-between items-center"
+            >
+              {{ displayDate(date) }}
+              <ChevronRightIcon class="w-5 h-5 text-neutral-500" />
+            </button>
+          </li>
+        </ol>
       </li>
     </ol>
   </Main>
