4fad3741bc668bbde4c30fd2d19dce6bbdff4a27
Indiana Kernick <indiana.kernick@gmail.com>
2023-04-01 18:56:54 +1030

    Add rounded favicon
    The rounded version looks a bit better as a favicon. Still using a non-rounded version for the app icon to let iOS apply the rounding properly.

--- client/icon.sh
+++ client/icon.sh
@@ -2,18 +2,18 @@
 
 # brew install librsvg pngcrush svgo
 
-svgo --multipass -o public/favicon.svg -i icon/icon.svg
+svgo --multipass -o public/favicon.svg -i icon/round.svg
 
 function rasterize {
-  rsvg-convert -h $2 -o public/$1-$2.png icon/icon.svg
-  pngcrush -brute -reduce -rem alla -ow public/$1-$2.png
+  rsvg-convert -h $3 -o public/$2-$3.png icon/$1.svg
+  pngcrush -brute -reduce -rem alla -ow public/$2-$3.png
 }
 
 for size in 32 64 128; do
-  rasterize favicon $size
+  rasterize round favicon $size
 done
 
 # https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html
 for size in 152 167 180; do
-  rasterize apple-touch-icon $size
+  rasterize default apple-touch-icon $size
 done
